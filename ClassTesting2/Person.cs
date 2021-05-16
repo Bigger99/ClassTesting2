9b2931aecea0725b81361d3d87a88a89e9739ca5
egork <egork@DESKTOP-QBJBKM0>
2021-05-16 17:40:09 +0300

    Add project files.

---
+++ ClassTesting2/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTesting2
+{
+    class Person
+    {
+
+        public string fullName;
+        public int age;
+
+        public void Move(string move) {
+            Console.WriteLine($"Персонаж {this.fullName} двигается {move}");
+        }
+
+        public void Talk(string talk)
+        {
+            Console.WriteLine($"Персонаж {this.fullName} сказал {talk}");
+        }
+
+        public Person() {
+            fullName = "";
+            age = 0;
+        }
+
+        public Person(string fullName, int age)
+        {
+            this.fullName = fullName;
+            this.age = age;
+        }
+    }
+}
---
+++ ClassTesting2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClassTesting2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Nick = new Person();
+            Nick.fullName = "Nick Borrow";
+            Nick.age = 20;
+
+            Person Bob = new Person("Bob Anderson", 34);
+
+            Nick.Move("вправо");
+            Nick.Talk("Привет");
+
+            Bob.Move("влево");
+            Bob.Talk("Пока");
+
+            Console.ReadKey();
+        }
+    }
+}
